fdff6a418b86b8643205711e12594a47103e2bb6
okensu <okensu.inbox@gmail.com>
2026-04-23 23:08:31 +0600

    fix: fix class name

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -92,7 +92,7 @@
 {#if !shouldShowNewEventsAutomatically}
 	<button
 	  onclick={() => loadNewEvents()}
-	  class="btn absolute top-2 left-1/2 -translate-x-1/2 text-sm {(newEvents.length > 0) ? 'bg-info text-base-100' : ''}"
+	  class="btn absolute top-2 left-1/2 -translate-x-1/2 text-sm {(newEvents.length > 0) ? 'btn-info text-base-100' : ''}"
 	>
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
 			<path fill-rule="evenodd" d="M15.312 11.424a5.5 5.5 0 0 1-9.201 2.466l-.312-.311h2.433a.75.75 0 0 0 0-1.5H3.989a.75.75 0 0 0-.75.75v4.242a.75.75 0 0 0 1.5 0v-2.43l.31.31a7 7 0 0 0 11.712-3.138.75.75 0 0 0-1.449-.39Zm1.23-3.723a.75.75 0 0 0 .219-.53V2.929a.75.75 0 0 0-1.5 0V5.36l-.31-.31A7 7 0 0 0 3.239 8.188a.75.75 0 1 0 1.448.389A5.5 5.5 0 0 1 13.89 6.11l.311.31h-2.432a.75.75 0 0 0 0 1.5h4.243a.75.75 0 0 0 .53-.219Z" clip-rule="evenodd" />
