127a2c50f2fc066445cd5e73fd3a6fb66d1afa30
okensu <okensu.inbox@gmail.com>
2026-04-23 15:07:55 +0600

    feat: add genemetrics logo; improve collapsed content logic

--- src/lib/features/events/Event.svelte
+++ src/lib/features/events/Event.svelte
@@ -1,19 +1,22 @@
 <script>
 	import { Highlight } from 'svelte-highlight';
 	import { json } from 'svelte-highlight/languages';
+	import genemetricsLogo from '$lib/assets/genemetrics.png';
 
-	let isUnknownEvent = true;
+	let isUnknownEvent = false;
 	let isPayloadVisible = $state(false);
 </script>
 
 <li class="list-row">
-	<div><img class="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/1@94.webp" alt="avatar" /></div>
+	<div><img class="size-10 rounded-box" src={genemetricsLogo} alt="Icon" /></div>
 	<div>
 		<div>GeneMetrics • ClickUp</div>
-		<div class="text-xs opacity-60">Daniel Luper posted a comment</div>
-	</div>
-	<div class="collapse list-col-wrap text-xs" class:collapse-open={isUnknownEvent || isPayloadVisible} class:collapse-close={!isUnknownEvent && !isPayloadVisible}>
-		<Highlight class="collapse-content" language={json} code={'{"hello": "world"}'} />
+		<div class="text-xs opacity-80">taskCommentPosted</div>
+		<div class="collapse text-xs" class:collapse-open={isUnknownEvent || isPayloadVisible} class:collapse-close={!isUnknownEvent && !isPayloadVisible}>
+			<div class="collapse-content transition-all" class:mt-4={isUnknownEvent || isPayloadVisible}>
+				<Highlight language={json} code={'{"hello": "world"}'} />
+			</div>
+		</div>
 	</div>
 	<button onclick={() => isPayloadVisible = !isPayloadVisible} class="btn btn-square btn-ghost {isPayloadVisible ? 'bg-primary text-base-100' : ''}" class:hidden={isUnknownEvent} aria-label="Payload">
 		<svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
