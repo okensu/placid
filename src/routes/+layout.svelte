e940b2cdd585edc128fdec6ca2d67e4ad67f6c02
okensu <okensu.inbox@gmail.com>
2026-04-23 12:15:31 +0600

    feat: update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-    import { webSocketState } from '$lib/features/websocket';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
 
 	let { children } = $props();
 </script>
@@ -10,9 +11,9 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{#if webSocketState.connectionStatus === 'connected'}
-  {@render children()}
-{:else}
+<!-- {#if webSocketState.connectionStatus === 'connected'} -->
+  
+<!-- {:else}
 	<main class="flex flex-col min-h-full justify-center items-center gap-4 fixed top-0 left-0 right-0 bottom-0">
 		{#if webSocketState.connectionStatus === 'connecting'}
 			<span class="loading loading-spinner loading-xl"></span>
@@ -30,4 +31,54 @@
 			</span>
 		{/if}
 	</main>
-{/if}
+{/if} -->
+
+<div class="drawer lg:drawer-open">
+  <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+
+  <div class="drawer-content">
+    <nav class="navbar w-full border-b border-neutral p-4 lg:hidden">
+      <label for="my-drawer-4" aria-label="open sidebar" class="btn btn-square btn-ghost">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor" class="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
+      </label>
+      <div class="px-4">Inbox</div>
+    </nav>
+    <div class="p-4">
+			{@render children()}
+    </div>
+  </div>
+
+  <div class="drawer-side is-drawer-close:overflow-visible">
+    <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
+    <div class="flex min-h-full flex-col items-start bg-base-200 border-r border-neutral is-drawer-close:w-16 is-drawer-open:w-64">
+      <ul class="menu w-full grow">
+        <li>
+          <button onclick={() => goto('/')} class="is-drawer-close:tooltip is-drawer-close:tooltip-right {(page.url.pathname === '/') ? 'bg-primary text-base-100' : ''}" data-tip="Inbox">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="my-1.5 inline-block size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H6.911a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661Z" />
+            </svg>
+            <span class="is-drawer-close:hidden">Inbox</span>
+          </button>
+        </li>
+
+        <li>
+          <button onclick={() => goto('/events')} class="is-drawer-close:tooltip is-drawer-close:tooltip-right {(page.url.pathname === '/events') ? 'bg-primary text-base-100' : ''}" data-tip="Events">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="my-1.5 inline-block size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z" />
+            </svg>
+            <span class="is-drawer-close:hidden">Queue</span>
+          </button>
+        </li>
+
+        <li>
+          <button onclick={() => goto('/prompt')} class="is-drawer-close:tooltip is-drawer-close:tooltip-right {(page.url.pathname === '/prompt') ? 'bg-primary text-base-100' : ''}" data-tip="Prompt">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="my-1.5 inline-block size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" />
+            </svg>
+            <span class="is-drawer-close:hidden">Terminal</span>
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
