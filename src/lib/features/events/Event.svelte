760e8e6d15971cafdae758bbe9343f8a15f60d68
okensu <okensu.inbox@gmail.com>
2026-04-23 14:04:27 +0600

    feat: always show the payload for unknown events

--- src/lib/features/events/Event.svelte
+++ src/lib/features/events/Event.svelte
@@ -12,10 +12,10 @@
 		<div>GeneMetrics • ClickUp</div>
 		<div class="text-xs opacity-60">Daniel Luper posted a comment</div>
 	</div>
-	<div class="collapse list-col-wrap text-xs" class:collapse-open={isPayloadVisible} class:collapse-close={!isPayloadVisible}>
+	<div class="collapse list-col-wrap text-xs" class:collapse-open={isUnknownEvent || isPayloadVisible} class:collapse-close={!isUnknownEvent && !isPayloadVisible}>
 		<Highlight class="collapse-content" language={json} code={'{"hello": "world"}'} />
 	</div>
-	<button onclick={() => isPayloadVisible = !isPayloadVisible} class="btn btn-square btn-ghost {isPayloadVisible ? 'bg-primary text-base-100' : ''}" aria-label="Payload">
+	<button onclick={() => isPayloadVisible = !isPayloadVisible} class="btn btn-square btn-ghost {isPayloadVisible ? 'bg-primary text-base-100' : ''}" class:hidden={isUnknownEvent} aria-label="Payload">
 		<svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
 			<path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
 		</svg>
